be246824154a67af113192e6c90d4ece9ee3f9dc
zw.xiong <zw.xiong@qq.com>
2024-09-27 15:55:52 +0800

    feat: 引入组件和自定义主题scss

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,40 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // element-plus 按需导入
-import AutoImport from 'unplugin-auto-import/vite'
+/* import AutoImport from 'unplugin-auto-import/vite' */
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers' 
+/* import ElementPlus from 'unplugin-element-plus/vite' */
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImport({
+     /* AutoImport({
       resolvers: [ElementPlusResolver()],
-    }),
+    }), */
     Components({
-      resolvers: [ElementPlusResolver()],
-    }),
+      resolvers: [ElementPlusResolver({
+        importStyle: "sass",
+        directives: true, 
+        version: "2.1.5",
+      }),
+    ],
+    }), 
+    /* ElementPlus({
+      useSource: true,
+    }), */
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
+    },
+  },
 })

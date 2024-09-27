f2fb4e0237e00b59156b9ae8ee607bea54fcea43
zw.xiong <zw.xiong@qq.com>
2024-09-27 18:09:11 +0800

    feat: 首页顶部布局

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,14 +1,15 @@
-<template>
-
-<div>我是首页
-  <RouterView></RouterView>
-</div>
-
-</template>
-
-<script setup lang='ts'>
-
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
 </script>
+
+<template>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template>
 <style scoped>
 
 </style>
